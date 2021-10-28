a756350c6646d7d55f79dd9078f109a95a14c70a
Spawn95 <disk-9@mail.ru>
2021-10-28 16:04:02 +0700

    Добавьте файлы проекта.

---
+++ Урок1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Урок1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Написание простой программы
+            Console.WriteLine("Напиши мне своё имя"); // Запрос имени
+            string userName = Console.ReadLine(); // Считывание параметра
+            var Date = DateTime.Now; // Значение даты
+            Console.WriteLine($"Привет, {userName}, сегодня {Date:d} !"); // Ответ на запрос
+        }
+    }
+}
